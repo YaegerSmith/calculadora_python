513f732853af4bc7aefd25a5c1f2dd3489178ae6
Esteban Vazquez <mestebanve123@gmail.com>
2024-01-11 13:31:28 -0800

    Se realizo el menu de opciones de la calculadora

---
+++ calculadora.py
@@ -0,0 +1,13 @@
+#Mensaje de bienvenida.
+print()
+print("Calculadora que suma, resta, multiplica y divide.")
+print()
+#Menu de opciones
+print("Eliga una de las opciones.")
+print("1.- Suma.")
+print("2.- Resta.")
+print("3.- Multiplicación.")
+print("4.- División.")
+print("5.- Salir.")
+opciones = int(print(input("Ingrese el número: ")))
+
