1ea6f2e6d9affc32ee147499ce95fe56a48b38c0
Esteban Vazquez <mestebanve123@gmail.com>
2024-01-11 14:23:55 -0800

    Se realizo el codigo para hacer la suma, resta, multiplicacion y division

--- calculadora.py
+++ calculadora.py
@@ -9,5 +9,51 @@ print("2.- Resta.")
 print("3.- Multiplicación.")
 print("4.- División.")
 print("5.- Salir.")
-opciones = int(print(input("Ingrese el número: ")))
+opciones = int(input("Ingrese el número: "))
+#Calcular la suma.
+if opciones == 1:
+    print("Eligio sumar.")
+    num1 = float(input("Ingrese el primer número: "))
+    num2 = float(input("Ingrese el segundo número: "))
+    print("La suma es igual a:", num1 + num2)
+    print()
+    print("Fin del programa.")
+    print()
+    exit()
+#Calcular la resta. 
+elif opciones == 2:
+    print("Eligio restar")
+    num1 = float(input("Ingrese el primer número: "))
+    num2 = float(input("Ingrese el segundo número: "))
+    print(num1 - num2)
+    print()
+    print("Fin del programa.")
+    print()
+    exit()
+#Calcular la multiplicación.
+elif opciones == 3:
+    print("Eligio multiplicar.")
+    num1 = float(input("Ingrese el primer número: "))
+    num2 = float(input("Ingrese el segundo número: "))
+    print(num1 * num2)
+    print()
+    print("Fin del programa.")
+    print()
+    exit()
+#Calcular la división.
+elif opciones == 4:
+    print("Eligio dividir.")
+    num1 = float(input("Ingrese el primer número: "))
+    num2 = float(input("Ingrese el segundo número: "))
+    print(num1 / num2)
+    print()
+    print("Fin del programa.")
+    print()
+    exit()
+#Mensaje de salida.
+elif opciones == 5:
+    print("Fin del programa, regrese pronto.")
+    print()
+    exit()
+
 
